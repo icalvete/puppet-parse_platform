4abfe464bbc55f3a23cb5a6fcc86f145f2142cc8
Npmorales <nperezmorales@yahoo.es>
2016-06-24 09:12:35 +0200

    Validate vars , default DB connection, download CloudCode repository

--- manifests/app.pp
+++ manifests/app.pp
@@ -1,12 +1,13 @@
 define  parse_platform::app (
 
-  $app_name       = $name,
-  $application_id = undef,
-  $master_key     = undef,
-  $port           = 1337,
-  $parse_root     = "/srv",
-  $cloud_code     = false
-
+  $app_name         = $name,
+  $application_id   = undef,
+  $master_key       = undef,
+  $port             = 1337,
+  $cloud_code       = false,
+  $database_URI     = undef,
+  $cloud_repository = undef,
+  $parse_root       = undef
 ) {
 
   if $cloud_code {
@@ -16,13 +17,25 @@ define  parse_platform::app (
 
   include parse_platform
 
-  validate_string($application_id)
+  if $app_name == undef {
+    fail('app_name must be a string')
+  }
+
+  if $application_id == undef {
+    fail('application_id must be a string')
+  }
 
   if $master_key == undef {
     fail('master_key must be a string')
   }
 
   validate_integer($port)
+
+  if $database_URI == undef {
+    warning('databaseURI not specified, falling back to localhost.')
+  }
+
+  validate_absolute_path($parse_root)
 
   file { "parse_root_${app_name}":
     ensure => directory,
@@ -40,12 +53,6 @@ define  parse_platform::app (
     mode   => '0755',
     require => File["parse_root_${app_name}"]
   }
-   file{"parse_main_${app_name}":
-    path    => "${parse_root}/${app_name}/cloud/main.js",
-    content => template("${module_name}/main.js.erb"),
-    mode    => '0644',
-    require => File["${parse_root}/${app_name}/cloud"]
-  }
 
   file{"parse_config_${app_name}":
     path    => "${parse_root}/${app_name}/config.json",
@@ -53,8 +60,7 @@ define  parse_platform::app (
     mode    => '0644',
     require => File["parse_root_${app_name}"]
   }
-  
-  
+
   upstart::job { "parse-server_${app_name}":
     description         => "parse-server_${$app_name}",
     start_on            => 'runlevel [2345]',
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,7 @@
 class parse_platform::install {
 
-
-package { 'parse-server':
-  provider => 'npm',
-  require  => Class['nodejs']
-}
+  package { 'parse-server':
+    provider => 'npm',
+    require  => Class['nodejs']
+  }
 }
