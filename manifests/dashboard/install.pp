abd63553d6f9bb8b15c1253cca0c8f58491702b9
Israel Calvete <icalvete@gmail.com>
2016-08-01 12:20:16 +0200

    setting parse version.

--- manifests/dashboard/install.pp
+++ manifests/dashboard/install.pp
@@ -1,6 +1,7 @@
 class parse_platform::dashboard::install {
 
   package { 'parse-dashboard':
+    ensure   => '1.0.14',
     provider => 'npm',
     require  => Class['nodejs']
   }
--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -1,6 +1,7 @@
 class parse_platform::server::install {
 
   package { 'parse-server':
+    ensure   => '2.2.16',
     provider => 'npm',
     require  => Class['nodejs']
   }
