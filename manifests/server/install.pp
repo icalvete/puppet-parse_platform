514c9ebf904210dd98b99bb8b9a758de4c027222
Israel Calvete <icalvete@gmail.com>
2016-08-30 09:04:41 +0200

    Updating to 2.2.18 with native bcrypt.

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -1,8 +1,14 @@
 class parse_platform::server::install {
 
   package { 'parse-server':
-    ensure   => '2.2.16',
+    ensure   => '2.2.18',
     provider => 'npm',
     require  => Class['nodejs']
+  }
+
+  package { 'bcrypt':
+    ensure   => '0.8.7',
+    provider => 'npm',
+    require  => Package['parse-server']
   }
 }
